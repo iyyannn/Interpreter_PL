cfc3579518f6f3a63de5ecd7d9591aba417b4069
iyyannn <logronio.315483@laspinas.sti.edu.ph>
2025-03-06 07:47:03 +0800

    interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -2,19 +2,18 @@ import re
 
 class Interpreter:
     def __init__(self, expression):
-        # Remove spaces from the input expression
+        
         self.expression = re.sub(r'\s+', '', expression)
         self.position = 0
         self.current_token = None
 
     def get_next_token(self):
-        # Check if we've reached the end of the expression
+        
         if self.position >= len(self.expression):
             return None
         
         current_char = self.expression[self.position]
         
-        # If it's a number, extract the full number
         if current_char.isdigit():
             number = ''
             while self.position < len(self.expression) and self.expression[self.position].isdigit():
@@ -22,7 +21,6 @@ class Interpreter:
                 self.position += 1
             return ('NUMBER', int(number))
         
-        # If it's an operator, return it
         if current_char in '+-':
             self.position += 1
             return ('OPERATOR', current_char)
@@ -30,7 +28,7 @@ class Interpreter:
         return None
 
     def evaluate_expression(self):
-        # Get the first number
+        
         self.current_token = self.get_next_token()
         
         if self.current_token and self.current_token[0] == 'NUMBER':
@@ -58,7 +56,6 @@ class Interpreter:
         
         return result
 
-# Test cases
 test_expressions = [
     "1 + 2 - 1",
     "10 - 5 - 2",
